397e5979e7092dde504372098133308cda9c194b
Sahil <sahilkumar6939@gmail.com>
2026-06-22 23:49:01 +0530

    add solution of one more leetcode question 493

--- Array/questions.js
+++ Array/questions.js
@@ -83,6 +83,18 @@ class questionOn2dArray {
     return result;
     // return this.count;
   }
+
+  Count_Reverse_Pairs(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] > 2 * nums[j]) {
+          this.count++;
+        }
+      }
+    }
+    return count;
+  }
 }
 const quesInstance = new questionOn2dArray();
 
