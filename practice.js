707abd2390f463efcfc1167eecd326fe9747e43d
Sahil <sahilkumar6939@gmail.com>
2026-07-11 23:47:24 +0530

    preparing for interview

--- practice.js
+++ practice.js
@@ -1,50 +1,10 @@
-// function* num() {
-//   console.log("first");
+let obj = {};
 
-//   yield 121;
+Object.defineProperty(obj, "name", {
+  value: "AS",
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
 
-//   console.log("second");
-
-//   yield 2;
-
-//   console.log("third");
-//   yield 22;
-// }
-
-// let iterator = num();
-
-// console.log(iterator.next());
-// console.log(iterator.return("dasf"));
-
-// console.log(iterator.next());
-// console.log(iterator.next());
-// console.log(iterator.next());
-
-let ar = [1, 2, 3, [4, 5, 6], 7, 8, 9];
-
-let obj = {
-  _name: "sahil",
-  get name() {
-    return this._name;
-  },
-
-  set name(name) {
-    this._name = name;
-  },
-  a: 1,
-  b: 2,
-  c: {
-    d: 3,
-    e: 4,
-  },
-};
-
-// console.log(obj.hasOwnProperty("a"));
-console.log(obj._name);
-// Object.preventExtensions(obj);
-// Object.freeze(obj);
-
-// how to unseal an object
-
-// obj.d = 34;
-// console.log(Object.getOwnPropertyDescriptor(obj, "a"));
+console.log(Object.getOwnPropertyDescriptors(obj));
